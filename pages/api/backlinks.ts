9fc30b003cda353657819a5ab40ad672fbd957a8
ngodichat <phamnguyenbinh90@gmail.com>
2023-03-04 10:51:08 +0700

    Add get method

--- pages/api/backlinks.ts
+++ pages/api/backlinks.ts
@@ -5,6 +5,7 @@ import fs from 'fs';
 import db from '../../database/database';
 import verifyUser from '../../utils/verifyUser';
 import BackLink from '../../database/models/backlink';
+import Domain from '../../database/models/domain';
 
 type BacklinksGetResponse = {
   backlinks?: BackLink[],
@@ -21,7 +22,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   await db.sync();
   const authorized = verifyUser(req, res);
   if (authorized !== 'authorized') {
-    res.status(401).json({ error: authorized });
+    return res.status(401).json({ error: authorized });
   }
 
   if (req.method === 'POST') {
@@ -30,11 +31,11 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     form.parse(req, async (err, fields, files: Record<string, any>) => {
       if (err) {
         console.error(err);
-        res.status(500).json({ message: 'Error parsing form data' });
+        return res.status(500).json({ message: 'Error parsing form data' });
       }
 
       if (!files || !files.csv) {
-        res.status(400).json({ message: 'CSV file is required' });
+        return res.status(400).json({ message: 'CSV file is required' });
       }
 
       const results: any[] = [];
@@ -54,6 +55,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
               domain_trust_flow: parseInt(row['Domain Trust Flow'], 10),
               domain_citation_flow: row['Domain Citation Flow'],
               link_first_index_date: row['Link First Indexed Date'],
+              domain: row['Target URL'],
             };
             records.push(record);
           });
@@ -63,29 +65,20 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
               updateOnDuplicate: ['anchor_text', 'source_trust_flow', 'source_citation_flow', 'domain_trust_flow', 'domain_citation_flow', 'link_first_index_date'],
             };
             await BackLink.bulkCreate(records, options);
-            res.status(200).json({ message: 'Upload file successfully' });
+            console.log('[SUCCESS] Adding New Backlinks successfully ');
+            return res.status(200).json({ message: 'Upload file successfully' });
           } catch (error) {
             console.log('[ERROR] Adding New Backlinks ', error);
-            res.status(400).json({ error: 'Could Not Add New Backlinks!' });
+            return res.status(400).json({ error: 'Could Not Add New Backlinks!' });
           }
         });
-      // fs.createReadStream(files.csv.path).on('data', (data) => console.log(data));
-      // console.log('files.csv.path: ', files.csv.filepath);
-      // const fileStream = fs.createReadStream(files.csv.filepath);
-      // fileStream.on('data', (data) => console.log(data));
-      // fileStream.close();
-      // fs.readFile(files.csv.path, (error, data) => {
-      //   if (error) throw error;
-      //   console.log(data);
-      // });
-      // res.status(200).json({ message: 'API POST endpoint reached' });
+        return null;
     });
   }
   if (req.method === 'GET') {
-    getBacklinks(req, res);
-  } else {
-    res.status(405).json({ message: 'Method not allowed' });
+    return getBacklinks(req, res);
   }
+  return res.status(405).json({ message: 'Method not allowed' });
 }
 
 const getBacklinks = async (req: NextApiRequest, res: NextApiResponse<BacklinksGetResponse>) => {
@@ -96,4 +89,27 @@ const getBacklinks = async (req: NextApiRequest, res: NextApiResponse<BacklinksG
   const domainObj: Domain | null = await Domain.findOne({ where: { slug: domainreq } });
   if (domainObj === null) return res.status(400).json({ error: 'Domain not found!' });
   const { domain } = domainObj;
+//   try {
+//     const allKeywords: BackLink[] = await BackLink.findAll({ where: { domain } });
+//     const keywords: KeywordType[] = parseKeywords(allKeywords.map((e) => e.get({ plain: true })));
+//     const processedKeywords = keywords.map((keyword) => {
+//        const historyArray = Object.keys(keyword.history).map((dateKey: string) => ({
+//           date: new Date(dateKey).getTime(),
+//           dateRaw: dateKey,
+//           position: keyword.history[dateKey],
+//        }));
+//        const historySorted = historyArray.sort((a, b) => a.date - b.date);
+//        const lastWeekHistory: KeywordHistory = {};
+//        historySorted.slice(-7).forEach((x: any) => { lastWeekHistory[x.dateRaw] = x.position; });
+//        const keywordWithSlimHistory = { ...keyword, lastResult: [], history: lastWeekHistory };
+//        const finalKeyword = domainSCData ? integrateKeywordSCData(keywordWithSlimHistory, domainSCData) : keywordWithSlimHistory;
+//        return finalKeyword;
+//     });
+//     return res.status(200).json({ keywords: processedKeywords });
+//  } catch (error) {
+//     console.log('[ERROR] Getting Domain Keywords for ', domain, error);
+//     return res.status(400).json({ error: 'Error Loading Keywords for this Domain.' });
+//  }
+
+  return res.status(200).json({ error: `Get backlinks successfully! ${domain}` });
 };
