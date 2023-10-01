df1ba0963de73ff8a463f34b09f5b72d538a9f9e
ngodichat <phamnguyenbinh90@gmail.com>
2023-10-01 09:46:13 +0700

    Add new keywords ignore option

--- components/keywords/AddKeywords.tsx
+++ components/keywords/AddKeywords.tsx
@@ -21,6 +21,8 @@ type KeywordsInput = {
 
 const AddKeywords = ({ closeModal, domain, keywords }: AddKeywordsProps) => {
    const [error, setError] = useState<string>('');
+   const [ignoreDuplicates, setIgnoreDuplicates] = useState<boolean>(false);
+   const [useExistingData, setUseExistingData] = useState<boolean>(false);
    const [newKeywordsData, setNewKeywordsData] = useState<KeywordsInput>({ keywords: '', device: 'desktop', country: 'US', domain, tags: '' });
    const { mutate: addMutate, isLoading: isAdding } = useAddKeywords(() => closeModal(false));
    const deviceTabStyle = 'cursor-pointer px-3 py-2 rounded mr-2';
@@ -30,14 +32,19 @@ const AddKeywords = ({ closeModal, domain, keywords }: AddKeywordsProps) => {
          const keywordsArray = [...new Set(newKeywordsData.keywords.split('\n').map((item) => item.trim()).filter((item) => !!item))];
          const currentKeywords = keywords.map((k) => `${k.keyword}-${k.device}-${k.country}`);
          const keywordExist = keywordsArray.filter((k) => currentKeywords.includes(`${k}-${newKeywordsData.device}-${newKeywordsData.country}`));
+         const { device, country, domain: kDomain, tags } = newKeywordsData;
+         let newKeywordsArray = keywordsArray.map((nItem) => ({ keyword: nItem, device, country, domain: kDomain, tags }));
          if (keywordExist.length > 0) {
-            setError(`Keywords ${keywordExist.join(',')} already Exist`);
-            setTimeout(() => { setError(''); }, 3000);
-         } else {
-            const { device, country, domain: kDomain, tags } = newKeywordsData;
-            const newKeywordsArray = keywordsArray.map((nItem) => ({ keyword: nItem, device, country, domain: kDomain, tags }));
-            addMutate(newKeywordsArray);
+            if (!ignoreDuplicates) {
+               setError(`Keywords ${keywordExist.join(',')} already Exist`);
+               setTimeout(() => { setError(''); }, 3000);
+               return;
+            }
+            newKeywordsArray = newKeywordsArray.filter((k: any) => !keywordExist.includes(k.keyword));
+            console.log('newKeywordsArray', keywordExist, currentKeywords, newKeywordsArray);
+            return;
          }
+         addMutate(newKeywordsArray);
       } else {
          setError('Please Insert a Keyword');
          setTimeout(() => { setError(''); }, 3000);
@@ -59,26 +66,26 @@ const AddKeywords = ({ closeModal, domain, keywords }: AddKeywordsProps) => {
 
                <div className=' my-3 flex justify-between text-sm'>
                   <div>
-                  <SelectField
-                     multiple={false}
-                     selected={[newKeywordsData.country]}
-                     options={Object.keys(countries).map((countryISO:string) => { return { label: countries[countryISO][0], value: countryISO }; })}
-                     defaultLabel='All Countries'
-                     updateField={(updated:string[]) => setNewKeywordsData({ ...newKeywordsData, country: updated[0] })}
-                     rounded='rounded'
-                     maxHeight={48}
-                     flags={true}
-                  />
+                     <SelectField
+                        multiple={false}
+                        selected={[newKeywordsData.country]}
+                        options={Object.keys(countries).map((countryISO: string) => { return { label: countries[countryISO][0], value: countryISO }; })}
+                        defaultLabel='All Countries'
+                        updateField={(updated: string[]) => setNewKeywordsData({ ...newKeywordsData, country: updated[0] })}
+                        rounded='rounded'
+                        maxHeight={48}
+                        flags={true}
+                     />
                   </div>
                   <ul className='flex text-xs font-semibold text-gray-500'>
                      <li
                         className={`${deviceTabStyle} ${newKeywordsData.device === 'desktop' ? '  bg-indigo-50 text-gray-700' : ''}`}
                         onClick={() => setNewKeywordsData({ ...newKeywordsData, device: 'desktop' })}
-                        ><Icon type='desktop' classes={'top-[3px]'} size={15} /> <i className='not-italic hidden lg:inline-block'>Desktop</i></li>
+                     ><Icon type='desktop' classes={'top-[3px]'} size={15} /> <i className='not-italic hidden lg:inline-block'>Desktop</i></li>
                      <li
                         className={`${deviceTabStyle} ${newKeywordsData.device === 'mobile' ? '  bg-indigo-50 text-gray-700' : ''}`}
                         onClick={() => setNewKeywordsData({ ...newKeywordsData, device: 'mobile' })}
-                        ><Icon type='mobile' /> <i className='not-italic hidden lg:inline-block'>Mobile</i></li>
+                     ><Icon type='mobile' /> <i className='not-italic hidden lg:inline-block'>Mobile</i></li>
                   </ul>
                </div>
 
@@ -92,18 +99,34 @@ const AddKeywords = ({ closeModal, domain, keywords }: AddKeywordsProps) => {
                   />
                   <span className='absolute text-gray-400 top-2 left-2'><Icon type="tags" size={16} /></span>
                </div>
+               <div className='flex'>
+                  <div className='flex-1'>
+                     <div className="mt-5 mb-3 text-sm font-medium text-gray-900 dark:text-gray-300">Ignore duplicates</div>
+                     <label className="relative inline-flex items-center cursor-pointer mb-8">
+                        <input type="checkbox" value="" className="sr-only peer" onChange={(event: any) => { setIgnoreDuplicates(event.target.checked) }} />
+                        <div className="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                     </label>
+                  </div>
+                  <div className='flex-1'>
+                     <div className="mt-5 mb-3 text-sm font-medium text-gray-900 dark:text-gray-300">Use existing data</div>
+                     <label className="relative inline-flex items-center cursor-pointer mb-8">
+                        <input type="checkbox" value="" className="sr-only peer" onChange={(event: any) => { setUseExistingData(event.target.checked) }} />
+                        <div className="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                     </label>
+                  </div>
+               </div>
             </div>
             {error && <div className='w-full mt-4 p-3 text-sm bg-red-50 text-red-700'>{error}</div>}
             <div className='mt-6 text-right text-sm font-semibold flex justify-between'>
                <button
                   className=' py-2 px-5 rounded cursor-pointer bg-indigo-50 text-slate-500 mr-3'
                   onClick={() => closeModal(false)}>
-                     Cancel
+                  Cancel
                </button>
                <button
                   className=' py-2 px-5 rounded cursor-pointer bg-blue-700 text-white'
                   onClick={() => !isAdding && addKeywords()}>
-                     {isAdding ? 'Adding....' : 'Add Keywords'}
+                  {isAdding ? 'Adding....' : 'Add Keywords'}
                </button>
             </div>
          </div>
